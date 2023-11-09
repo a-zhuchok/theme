51a1aac9d7f0c0b87953d6f986a23630350cc719
Anna <zhukovaanna111@gmail.com>
2023-11-09 15:42:52 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import List from "./List"
+import List from "./List";
 import './App.css';
-import React, {useState,  useRef } from 'react'
+import React, {useState,  useRef } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import Switch from './Switch';
 
+const AppWrapper = styled.div`
+width: 100%;
+min-height:100vh;
+display:flex;
+flex-direction: column;
+justify-content:center;
+align-items: center;
+background-color: ${(props)=>props.theme.body}
+`
+const Insert = styled.div`
+margin:1rem;`
+
+const ButtonFocus = styled.div`
+background-color: ${(props)=>props.theme.buttom};
+cursor: pointer;
+padding: 5px;
+`
+const Text = styled.div`
+color: ${(props)=>props.theme.text};
+margin:1rem;
+`
+const darkTheme = {
+  body:"#1c1c1c",
+  buttom:"rgb(247, 165, 13)",
+  text:"#b6b6b6",
+  }
+const lightTheme = {
+  body:"#fff",
+  buttom:"#fff",
+  text:"#black",
+  }
 
 function App() {
+  const [theme, setTheme]=useState('light');
+  const isDarkTheme = theme ==="dark";
+  const toggleTheme = ()=>{
+    setTheme(isDarkTheme ? "light" : "dark")}
   const [text, setText] = useState('')
   const [ar, setAr] = useState([1,333])
   const textInput = useRef(0)
@@ -20,15 +57,17 @@ function App() {
         setText("")
     }}
     
-    
   return (
-    <div>
-        <input ref={textInput} value={text} onChange ={handleChange} onKeyDown={handleKeyDown}/>
-            <button onClick={() => valid()}>
-                Click to focus
-            </button>
-        <List arr={ar} />   
-    </div>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <AppWrapper>
+        <Insert><input ref={textInput} value={text} onChange ={handleChange} onKeyDown={handleKeyDown}/></Insert>
+        <button onClick={() => valid()}>
+        <ButtonFocus>Click to focus </ButtonFocus>
+        </button>
+        <Text><List color={(props)=>props.theme.buttom} arr={ar} /> </Text> 
+        <Switch toggleTheme={toggleTheme} ></Switch>
+      </AppWrapper>  
+    </ThemeProvider>
   );
 }
 
--- src/List.js
+++ src/List.js
@@ -1,16 +1,26 @@
-import {useState} from 'react';
-const List = ({arr})=>{
-   
-    const [isShowPrefix, setIsShowPrefix] = useState(false);
+import { useState } from 'react';
+import styled from 'styled-components';
 
+const Lists = styled.div`
+display:flex;
+flex-direction: column;
+justify-content:center;
+align-items: center;
+`
+const ButtonAdd = styled.div`
+padding:5px;
+background-color: ${(props)=>props.theme.buttom};
+cursor: pointer;
+`
+const List = ({arr}) => {
+    const [isShowPrefix, setIsShowPrefix] = useState(false);
     const handlerButtonClick = () => {
         setIsShowPrefix(!isShowPrefix);
     }
-
-    return (<div>
-        <ul>{arr.map((value, index)=><li key={index}>{`${isShowPrefix ? '!!!': ""}${value}`}</li>)}</ul>
-        <button onClick={handlerButtonClick}>Add !!!</button>
-        </div>
+    return (<Lists>
+        <ul>{arr.map((value, index) => <li key={index}>{`${isShowPrefix ? '!!!' : ""}${value}`}</li>)}</ul>
+        <button onClick={handlerButtonClick}><ButtonAdd>Add !!!</ButtonAdd></button>
+    </Lists>
     )
 }
 export default List
